8a239fe58d368ed862867337778c8e9802790029
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2019-03-09 22:52:51 +0100

    Adds support for Bitbucket Server

---
+++ bitbucket-server/bitbucketserver.go
@@ -0,0 +1,218 @@
+package bitbucketserver
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	ErrEventNotSpecifiedToParse  = errors.New("no Event specified to parse")
+	ErrInvalidHTTPMethod         = errors.New("invalid HTTP Method")
+	ErrMissingEventKeyHeader     = errors.New("missing X-Event-Key Header")
+	ErrMissingHubSignatureHeader = errors.New("missing X-Hub-Signature Header")
+	ErrEventNotFound             = errors.New("event not defined to be parsed")
+	ErrParsingPayload            = errors.New("error parsing payload")
+	ErrHMACVerificationFailed    = errors.New("HMAC verification failed")
+)
+
+type Event string
+
+const (
+	RepositoryReferenceChangedEvent Event = "repo:refs_changed"
+	RepositoryModifiedEvent         Event = "repo:modified"
+	RepositoryForkedEvent           Event = "repo:forked"
+	RepositoryCommentAddedEvent     Event = "repo:comment:added"
+	RepositoryCommentEditedEvent    Event = "repo:comment:edited"
+	RepositoryCommentDeletedEvent   Event = "repo:comment:deleted"
+
+	PullRequestOpenedEvent   Event = "pr:opened"
+	PullRequestModifiedEvent Event = "pr:modified"
+	PullRequestMergedEvent   Event = "pr:merged"
+	PullRequestDeclinedEvent Event = "pr:declined"
+	PullRequestDeletedEvent  Event = "pr:deleted"
+
+	PullRequestReviewerUpdatedEvent    Event = "pr:reviewer:updated"
+	PullRequestReviewerApprovedEvent   Event = "pr:reviewer:approved"
+	PullRequestReviewerUnapprovedEvent Event = "pr:reviewer:unapproved"
+	PullRequestReviewerNeedsWorkEvent  Event = "pr:reviewer:needs_work"
+
+	PullRequestCommentAddedEvent   Event = "pr:comment:added"
+	PullRequestCommentEditedEvent  Event = "pr:comment:edited"
+	PullRequestCommentDeletedEvent Event = "pr:comment:deleted"
+
+	DiagnosticsPingEvent Event = "diagnostics:ping"
+)
+
+// Option is a configuration option for the webhook
+type Option func(*Webhook) error
+
+// Options is a namespace var for configuration options
+var Options = WebhookOptions{}
+
+// WebhookOptions is a namespace for configuration option methods
+type WebhookOptions struct{}
+
+// Secret registers the GitHub secret
+func (WebhookOptions) Secret(secret string) Option {
+	return func(hook *Webhook) error {
+		hook.secret = secret
+		return nil
+	}
+}
+
+// Webhook instance contains all methods needed to process events
+type Webhook struct {
+	secret string
+}
+
+// New creates and returns a WebHook instance denoted by the Provider type
+func New(options ...Option) (*Webhook, error) {
+	hook := new(Webhook)
+	for _, opt := range options {
+		if err := opt(hook); err != nil {
+			return nil, errors.New("Error applying Option")
+		}
+	}
+	return hook, nil
+}
+
+func (hook *Webhook) Parse(r *http.Request, events ...Event) (interface{}, error) {
+	defer func() {
+		_, _ = io.Copy(ioutil.Discard, r.Body)
+		_ = r.Body.Close()
+	}()
+
+	if len(events) == 0 {
+		return nil, ErrEventNotSpecifiedToParse
+	}
+
+	if r.Method != http.MethodPost {
+		return nil, ErrInvalidHTTPMethod
+	}
+
+	event := r.Header.Get("X-Event-Key")
+	if event == "" {
+		return nil, ErrMissingEventKeyHeader
+	}
+
+	bitbucketEvent := Event(event)
+
+	var found bool
+	for _, evt := range events {
+		if evt == bitbucketEvent {
+			found = true
+			break
+		}
+	}
+	// event not defined to be parsed
+	if !found {
+		return nil, ErrEventNotFound
+	}
+
+	if bitbucketEvent == DiagnosticsPingEvent {
+		return DiagnosticsPingPayload{}, nil
+	}
+
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil || len(payload) == 0 {
+		return nil, ErrParsingPayload
+	}
+
+	if len(hook.secret) > 0 {
+		signature := r.Header.Get("X-Hub-Signature")
+		if len(signature) == 0 {
+			return nil, ErrMissingHubSignatureHeader
+		}
+		mac := hmac.New(sha256.New, []byte(hook.secret))
+		_, _ = mac.Write(payload)
+		expectedMAC := hex.EncodeToString(mac.Sum(nil))
+
+		if !hmac.Equal([]byte(signature[7:]), []byte(expectedMAC)) {
+			return nil, ErrHMACVerificationFailed
+		}
+	}
+
+	switch bitbucketEvent {
+	case RepositoryReferenceChangedEvent:
+		var pl RepositoryReferenceChangedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryModifiedEvent:
+		var pl RepositoryModifiedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryForkedEvent:
+		var pl RepositoryForkedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryCommentAddedEvent:
+		var pl RepositoryCommentAddedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryCommentEditedEvent:
+		var pl RepositoryCommentEditedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryCommentDeletedEvent:
+		var pl RepositoryCommentDeletedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestOpenedEvent:
+		var pl PullRequestOpenedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestModifiedEvent:
+		var pl PullRequestModifiedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestMergedEvent:
+		var pl PullRequestMergedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestDeclinedEvent:
+		var pl PullRequestDeclinedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestDeletedEvent:
+		var pl PullRequestDeletedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewerUpdatedEvent:
+		var pl PullRequestReviewerUpdatedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewerApprovedEvent:
+		var pl PullRequestReviewerApprovedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewerUnapprovedEvent:
+		var pl PullRequestReviewerUnapprovedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewerNeedsWorkEvent:
+		var pl PullRequestReviewerNeedsWorkPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestCommentAddedEvent:
+		var pl PullRequestCommentAddedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestCommentEditedEvent:
+		var pl PullRequestCommentEditedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestCommentDeletedEvent:
+		var pl PullRequestCommentDeletedPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	default:
+		return nil, fmt.Errorf("unknown event %s", bitbucketEvent)
+	}
+}
---
+++ bitbucket-server/payload.go
@@ -0,0 +1,273 @@
+package bitbucketserver
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type DiagnosticsPingPayload struct{}
+
+type RepositoryReferenceChangedPayload struct {
+	Date       Date               `json:"date"`
+	EventKey   Event              `json:"eventKey"`
+	Actor      User               `json:"actor"`
+	Repository Repository         `json:"repository"`
+	Changes    []RepositoryChange `json:"changes"`
+}
+
+type RepositoryModifiedPayload struct {
+	Date     Date       `json:"date"`
+	EventKey Event      `json:"eventKey"`
+	Actor    User       `json:"actor"`
+	Old      Repository `json:"old"`
+	New      Repository `json:"new"`
+}
+
+type RepositoryForkedPayload struct {
+	Date       Date       `json:"date"`
+	EventKey   Event      `json:"eventKey"`
+	Actor      User       `json:"actor"`
+	Repository Repository `json:"repository"`
+}
+
+type RepositoryCommentAddedPayload struct {
+	Date       Date       `json:"date"`
+	EventKey   Event      `json:"eventKey"`
+	Actor      User       `json:"actor"`
+	Comment    Comment    `json:"comment"`
+	Repository Repository `json:"repository"`
+	Commit     string     `json:"commit"`
+}
+
+type RepositoryCommentEditedPayload struct {
+	Date            Date       `json:"date"`
+	EventKey        Event      `json:"eventKey"`
+	Actor           User       `json:"actor"`
+	Comment         Comment    `json:"comment"`
+	PreviousComment string     `json:"previousComment"`
+	Repository      Repository `json:"repository"`
+	Commit          string     `json:"commit"`
+}
+
+type RepositoryCommentDeletedPayload struct {
+	Date       Date       `json:"date"`
+	EventKey   Event      `json:"eventKey"`
+	Actor      User       `json:"actor"`
+	Comment    Comment    `json:"comment"`
+	Repository Repository `json:"repository"`
+	Commit     string     `json:"commit"`
+}
+
+type PullRequestOpenedPayload struct {
+	Date        Date        `json:"date"`
+	EventKey    Event       `json:"eventKey"`
+	Actor       User        `json:"actor"`
+	PullRequest PullRequest `json:"pullRequest"`
+}
+
+type PullRequestModifiedPayload struct {
+	Date                Date                   `json:"date"`
+	EventKey            Event                  `json:"eventKey"`
+	Actor               User                   `json:"actor"`
+	PullRequest         PullRequest            `json:"pullRequest"`
+	PreviousTitle       string                 `json:"previousTitle"`
+	PreviousDescription string                 `json:"previousDescription"`
+	PreviousTarget      map[string]interface{} `json:"previousTarget"`
+}
+
+type PullRequestMergedPayload struct {
+	Date        Date        `json:"date"`
+	EventKey    Event       `json:"eventKey"`
+	Actor       User        `json:"actor"`
+	PullRequest PullRequest `json:"pullRequest"`
+}
+
+type PullRequestDeclinedPayload struct {
+	Date        Date        `json:"date"`
+	EventKey    Event       `json:"eventKey"`
+	Actor       User        `json:"actor"`
+	PullRequest PullRequest `json:"pullRequest"`
+}
+
+type PullRequestDeletedPayload struct {
+	Date        Date        `json:"date"`
+	EventKey    Event       `json:"eventKey"`
+	Actor       User        `json:"actor"`
+	PullRequest PullRequest `json:"pullRequest"`
+}
+
+type PullRequestReviewerUpdatedPayload struct {
+	Date             Date        `json:"date"`
+	EventKey         Event       `json:"eventKey"`
+	Actor            User        `json:"actor"`
+	PullRequest      PullRequest `json:"pullRequest"`
+	RemovedReviewers []User      `json:"removedReviewers"`
+	AddedReviewers   []User      `json:"addedReviewers"`
+}
+
+type PullRequestReviewerApprovedPayload struct {
+	Date           Date                   `json:"date"`
+	EventKey       Event                  `json:"eventKey"`
+	Actor          User                   `json:"actor"`
+	PullRequest    PullRequest            `json:"pullRequest"`
+	Participant    PullRequestParticipant `json:"participant"`
+	PreviousStatus string                 `json:"previousStatus"`
+}
+
+type PullRequestReviewerUnapprovedPayload struct {
+	Date           Date                   `json:"date"`
+	EventKey       Event                  `json:"eventKey"`
+	Actor          User                   `json:"actor"`
+	PullRequest    PullRequest            `json:"pullRequest"`
+	Participant    PullRequestParticipant `json:"participant"`
+	PreviousStatus string                 `json:"previousStatus"`
+}
+
+type PullRequestReviewerNeedsWorkPayload struct {
+	Date           Date                   `json:"date"`
+	EventKey       Event                  `json:"eventKey"`
+	Actor          User                   `json:"actor"`
+	PullRequest    PullRequest            `json:"pullRequest"`
+	Participant    PullRequestParticipant `json:"participant"`
+	PreviousStatus string                 `json:"previousStatus"`
+}
+
+type PullRequestCommentAddedPayload struct {
+	Date            Date        `json:"date"`
+	EventKey        Event       `json:"eventKey"`
+	Actor           User        `json:"actor"`
+	PullRequest     PullRequest `json:"pullRequest"`
+	Comment         Comment     `json:"comment"`
+	CommentParentId uint64      `json:"commentParentId,omitempty"`
+}
+
+type PullRequestCommentEditedPayload struct {
+	Date            Date        `json:"date"`
+	EventKey        Event       `json:"eventKey"`
+	Actor           User        `json:"actor"`
+	PullRequest     PullRequest `json:"pullRequest"`
+	Comment         Comment     `json:"comment"`
+	CommentParentId string      `json:"commentParentId,omitempty"`
+	PreviousComment string      `json:"previousComment"`
+}
+
+type PullRequestCommentDeletedPayload struct {
+	Date            Date        `json:"date"`
+	EventKey        Event       `json:"eventKey"`
+	Actor           User        `json:"actor"`
+	PullRequest     PullRequest `json:"pullRequest"`
+	Comment         Comment     `json:"comment"`
+	CommentParentId uint64      `json:"commentParentId,omitempty"`
+}
+
+// -----------------------
+
+type User struct {
+	ID           uint64                 `json:"id"`
+	Name         string                 `json:"name"`
+	EmailAddress string                 `json:"emailAddress"`
+	DisplayName  string                 `json:"displayName"`
+	Active       bool                   `json:"active"`
+	Slug         string                 `json:"slug"`
+	Type         string                 `json:"type"`
+	Links        map[string]interface{} `json:"links"`
+}
+
+type Repository struct {
+	ID            uint64                 `json:"id"`
+	Slug          string                 `json:"slug"`
+	Name          string                 `json:"name"`
+	ScmId         string                 `json:"scmId"`
+	State         string                 `json:"state"`
+	StatusMessage string                 `json:"statusMessage"`
+	Forkable      bool                   `json:"forkable"`
+	Origin        *Repository            `json:"origin,omitempty"`
+	Project       Project                `json:"project"`
+	Public        bool                   `json:"public"`
+	Links         map[string]interface{} `json:"links"`
+}
+
+type Project struct {
+	ID     uint64                 `json:"id"`
+	Key    string                 `json:"key"`
+	Name   string                 `json:"name"`
+	Type   string                 `json:"type"`
+	Public *bool                  `json:"public,omitempty"`
+	Owner  User                   `json:"owner"`
+	Links  map[string]interface{} `json:"links"`
+}
+
+type PullRequest struct {
+	ID           uint64                   `json:"id"`
+	Version      uint64                   `json:"version"`
+	Title        string                   `json:"title"`
+	Description  string                   `json:"description,omitempty"`
+	State        string                   `json:"state"`
+	Open         bool                     `json:"open"`
+	Closed       bool                     `json:"closed"`
+	CreatedDate  uint64                   `json:"createdDate"`
+	UpdatedDate  uint64                   `json:"updatedDate,omitempty"`
+	ClosedDate   uint64                   `json:"closedDate,omitempty"`
+	FromRef      RepositoryReference      `json:"fromRef"`
+	ToRef        RepositoryReference      `json:"toRef"`
+	Locked       bool                     `json:"locked"`
+	Author       PullRequestParticipant   `json:"author"`
+	Reviewers    []PullRequestParticipant `json:"reviewers"`
+	Participants []PullRequestParticipant `json:"participants"`
+	Properties   map[string]interface{}   `json:"properties,omitempty"`
+	Links        map[string]interface{}   `json:"links"`
+}
+
+type RepositoryChange struct {
+	Reference   RepositoryReference `json:"ref"`
+	ReferenceId string              `json:"refId"`
+	FromHash    string              `json:"fromHash"`
+	ToHash      string              `json:"toHash"`
+	Type        string              `json:"type"`
+}
+
+type RepositoryReference struct {
+	ID           string     `json:"id"`
+	DisplayId    string     `json:"displayId"`
+	Type         string     `json:"type,omitempty"`
+	LatestCommit string     `json:"latestCommit,omitempty"`
+	Repository   Repository `json:"repository,omitempty"`
+}
+
+type Comment struct {
+	ID                  uint64                   `json:"id"`
+	Properties          map[string]interface{}   `json:"properties,omitempty"`
+	Version             uint64                   `json:"version"`
+	Text                string                   `json:"text"`
+	Author              User                     `json:"author"`
+	CreatedDate         uint64                   `json:"createdDate"`
+	UpdatedDate         uint64                   `json:"updatedDate"`
+	Comments            []map[string]interface{} `json:"comments"`
+	Tasks               []map[string]interface{} `json:"tasks"`
+	PermittedOperations map[string]interface{}   `json:"permittedOperations,omitempty"`
+}
+
+type PullRequestParticipant struct {
+	User               User   `json:"user"`
+	LastReviewedCommit string `json:"lastReviewedCommit,omitempty"`
+	Role               string `json:"role"`
+	Approved           bool   `json:"approved"`
+	Status             string `json:"status"`
+}
+
+type Date time.Time
+
+func (b *Date) UnmarshalJSON(p []byte) error {
+	t, err := time.Parse("2006-01-02T15:04:05Z0700", strings.Replace(string(p), "\"", "", -1))
+	if err != nil {
+		return err
+	}
+	*b = Date(t)
+	return nil
+}
+
+func (b Date) MarshalJSON() ([]byte, error) {
+	stamp := fmt.Sprintf("\"%s\"", time.Time(b).Format("2006-01-02T15:04:05Z0700"))
+	return []byte(stamp), nil
+}
