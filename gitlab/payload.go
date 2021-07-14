8618bb2b63cb14c53d394052f264a973c9385cf1
Zhdanov_R <zhda@1c.ru>
2021-07-14 14:46:48 +0300

    add Runner to  BuildEventPayload struct

--- gitlab/payload.go
+++ gitlab/payload.go
@@ -150,6 +150,7 @@ type BuildEventPayload struct {
 	User              User        `json:"user"`
 	Commit            BuildCommit `json:"commit"`
 	Repository        Repository  `json:"repository"`
+	Runner            Runner      `json:"runner"`
 }
 
 // JobEventPayload contains the information for GitLab's Job status change
