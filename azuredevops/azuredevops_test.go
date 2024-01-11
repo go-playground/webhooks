febd18b1a2fe1c6007b56c74bf945c784f95f305
raul <raul.cabello@suse.com>
2024-01-11 15:15:14 +0100

    Add azure basic auth verification

--- azuredevops/azuredevops_test.go
+++ azuredevops/azuredevops_test.go
@@ -1,6 +1,9 @@
 package azuredevops
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -115,5 +118,55 @@ func TestWebhooks(t *testing.T) {
 			assert.NoError(parseError)
 			assert.Equal(reflect.TypeOf(tc.typ), reflect.TypeOf(results))
 		})
+	}
+}
+
+func TestParseBasicAuth(t *testing.T) {
+	const validUser = "validUser"
+	const validPass = "pass123"
+	tests := []struct {
+		name        string
+		webhookUser string
+		webhookPass string
+		reqUser     string
+		reqPass     string
+		expectedErr error
+	}{
+		{
+			name:        "valid basic auth",
+			webhookUser: validUser,
+			webhookPass: validPass,
+			reqUser:     validUser,
+			reqPass:     validPass,
+			expectedErr: fmt.Errorf("unknown event "), // no event passed, so this is expected
+		},
+		{
+			name:        "no basic auth provided",
+			expectedErr: fmt.Errorf("unknown event "), // no event passed, so this is expected
+		},
+		{
+			name:        "invalid basic auth",
+			webhookUser: validUser,
+			webhookPass: validPass,
+			reqUser:     "fakeUser",
+			reqPass:     "fakePass",
+			expectedErr: ErrBasicAuthVerificationFailed,
+		},
+	}
+
+	for _, tt := range tests {
+		h := Webhook{
+			username: tt.webhookUser,
+			password: tt.webhookPass,
+		}
+		body := []byte(`{}`)
+		r, err := http.NewRequest(http.MethodPost, "", bytes.NewBuffer(body))
+		assert.NoError(t, err)
+		r.SetBasicAuth(tt.reqUser, tt.reqPass)
+
+		p, err := h.Parse(r)
+
+		assert.Equal(t, err, tt.expectedErr)
+		assert.Nil(t, p)
 	}
 }
